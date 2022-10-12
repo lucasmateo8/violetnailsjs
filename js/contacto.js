fbb92660eae7bab28a0071d972e4d9aea2e4775a
lucasmateo8 <mateolucas990@gmail.com>
2022-10-11 23:10:38 -0300

    finalizandoproyecto

---
+++ js/contacto.js
@@ -0,0 +1,9 @@
+
+const form = document.querySelector("#contacto");
+
+const enviarFormulario = (event) => {
+   alert("Su consulta ha sido enviada correctamente.");
+};
+
+
+form.addEventListener("submit",enviarFormulario);
